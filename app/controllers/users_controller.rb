c3a267b093a6f1f89913f71a9f8cf775e00961e5
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-04-05 08:34:56 +0900

    ユーザー登録後画面設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -6,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user.id)
     else
       render :new
     end
