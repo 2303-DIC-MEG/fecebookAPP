50bdee7d90dd93e8faf93e2a08ae37ecc37c1e74
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-04-05 09:17:59 +0900

    pictureモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_230711) do
+ActiveRecord::Schema.define(version: 2023_04_05_001643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_04_04_230711) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pictures", "users"
 end
